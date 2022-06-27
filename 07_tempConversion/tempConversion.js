0274996fc70151c0e1236638a57fadde4e7a136d
Will <willbond1@gmail.com>
2022-06-27 16:23:53 -0400

    Finished js/06

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let rawNumber = (tempF - 32) * (5/9);
+  return Math.round(rawNumber * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let rawNumber = tempC * (9/5) + 32;
+  return Math.round(rawNumber * 10) / 10;
 };
 
 // Do not edit below this line

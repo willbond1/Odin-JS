5c12efb4b6c0dbdc758901dc0c5bcc03aff4dd34
Will <willbond1@gmail.com>
2022-06-27 14:35:59 -0400

    Finished js/04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArray = str.split("");
+    let strResult = "";
+    for(let i = 0; i < str.length; i++)
+        strResult += strArray.pop();
+    return strResult;
 };
 
 // Do not edit below this line

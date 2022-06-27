07dee8515bf2e939fe82a78e1e4d7148c0b07326
Will <willbond1@gmail.com>
2022-06-27 14:19:19 -0400

    Finished js/02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, numTimes) {
+    if(numTimes < 0)
+        return "ERROR";
+    let resultStr = "";
+    for(let i = 0; i < numTimes; i++)
+        resultStr += str;
+    return resultStr;
 };
 
 // Do not edit below this line

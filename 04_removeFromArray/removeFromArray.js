60004e67d9ece4eedf03a86b7e40e04b5742c896
Will <willbond1@gmail.com>
2022-06-27 15:42:24 -0400

    Finished js/04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    let localArr = arr;
+    for(let item of toRemove) {
+        let itemIndex = localArr.indexOf(item);
+        if(itemIndex === -1)
+            continue;
+        localArr.splice(itemIndex, 1);
+    }
+    return localArr;
 };
 
 // Do not edit below this line

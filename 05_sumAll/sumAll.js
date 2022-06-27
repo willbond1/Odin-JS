063470877485a298720b9ea8c96b5ad44c357c41
Will <willbond1@gmail.com>
2022-06-27 16:04:16 -0400

    Finished js/05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(firstN, secondN) {
+    if(firstN < 0 || secondN < 0 ||
+        typeof(firstN) !== "number" || typeof(secondN) !== "number")
+        return "ERROR";
 
+    let sum = 0;
+    let beg = Math.min(firstN, secondN);
+    let end = Math.max(firstN, secondN);
+
+    for(let i = beg; i <= end; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line

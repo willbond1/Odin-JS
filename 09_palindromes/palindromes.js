ccdd2947155f5d96131c5406c7ee31ad2564be29
Will <willbond1@gmail.com>
2022-06-29 10:41:22 -0400

    Finished js/09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const alphaRegex = /[a-z]/;
 
+function arraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+        return false;
+    for(let i in arr1)
+        if(arr1[i] !== arr2[i])
+            return false;
+    return true;
+}
+
+const palindromes = function (str) {
+    let charArray = str.toLowerCase().split("");
+    charArray = charArray.filter(char => alphaRegex.test(char));
+    let reverseArray = charArray.slice().reverse();
+    return arraysEqual(charArray, reverseArray);
 };
 
 // Do not edit below this line

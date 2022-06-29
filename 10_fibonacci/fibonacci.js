9985ae72ae6503a5e670c07d15d103828fe01a53
Will <willbond1@gmail.com>
2022-06-29 10:50:17 -0400

    Finished js/10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0)
+        return "OOPS";
 
+    let result = 0;
+    let prevOne = 1;
+    let prevTwo = 0;
+
+    for(let i = 0; i < n; i++) {
+        prevTwo = prevOne;
+        prevOne = result;
+        result = prevOne + prevTwo;
+    }
+    return result;
 };
 
 // Do not edit below this line

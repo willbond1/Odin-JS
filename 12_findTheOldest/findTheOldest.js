bb64d490885db93ddf0808324cc4256beecfad47
Will <willbond1@gmail.com>
2022-06-29 13:22:59 -0400

    Finished js/12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if(oldest.yearOfDeath === undefined)
+            oldest.yearOfDeath = currentYear;
+        if(person.yearOfDeath === undefined)
+            person.yearOfDeath = currentYear;
+        
+        const personAge = person.yearOfDeath - person.yearOfBirth;
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+
+        if(personAge > oldestAge)
+            return person
+        else
+            return oldest;
+    });
 };
 
 // Do not edit below this line

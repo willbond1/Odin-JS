e7ba323de5884acb3a191774efa7a35faf4d09b2
Will <willbond1@gmail.com>
2022-06-29 09:49:27 -0400

    Finished js/08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(let num of arr)
+    sum += num;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for(let num of arr)
+    result *= num;
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let result = 1;
+  for(let i = 0; i < y; i++)
+    result *= x;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for(let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
